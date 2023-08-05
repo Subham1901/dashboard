786bf1faa6e26a4602fd9a9a07d774f64b2797ee
Subham Prasad Panda <79370554+Subham1901@users.noreply.github.com>
2023-08-05 19:56:54 +0530

    map updated

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -112,7 +112,7 @@ const Profile = () => {
                 style={{ border: 0 }}
                 allowfullscreen=""
                 loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
+                referrerPolicy="no-referrer-when-downgrade"
               ></iframe>
             </>
             <div className="map-content">
